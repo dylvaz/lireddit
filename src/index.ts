48aacb007ba78dbd1446a7985656a1bbf823f915
dylvaz <vazquezadylan@gmail.com>
2021-03-14 18:23:48 -0700

    implemented mikrrOrm setup - initialize the ORM - passed the mikroOrmConfig in the options paramter. - connect to the database - get migrations up to the latest

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('hello world');
+import { MikroORM } from '@mikro-orm/core';
+
+import { _prod_ } from './constants';
+import mikroOrmConfig from './mikro-orm.config';
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroOrmConfig);
+  await orm.getMigrator().up();
+};
+
+main().catch((err) => {
+  console.error(err);
+});
