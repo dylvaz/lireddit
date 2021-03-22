760b4efbd12e2f3c1c7b2e5f30a5c2d396198160
dylvaz <vazquezadylan@gmail.com>
2021-03-21 21:48:50 -0700

    added cors middleware and the frontend url as the origin

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import { buildSchema } from 'type-graphql';
 import redis from 'redis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
+import cors from 'cors';
 
 import { PostResolver } from './graphql/resolvers/postResolvers';
 import { _prod_ } from './constants';
@@ -24,6 +25,7 @@ const main = async () => {
 
   const RedisStore = connectRedis(session);
   const redisClient = redis.createClient();
+  app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
   app.use(
     session({
@@ -49,7 +51,7 @@ const main = async () => {
     context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
   });
 
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({ app, cors: false });
 
   app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT} 👻`);
