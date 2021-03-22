a117feafccb7bd4a8a6bacdda2f33d005a906d52
dylvaz <vazquezadylan@gmail.com>
2021-03-21 21:34:59 -0700

    created a dynamic input by using formik's useField hook
    - creates a formcontrol that is invalid if the field has any errors from formiks meta data
    - creates a label from the input
    - creates an input for the field and props minus label and size
    - displays an error message if there is one to be displayed

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -1,25 +1,31 @@
-import React from 'react'
-import { FormControl, FormLabel, Input } from '@chakra-ui/react';
+import React from 'react';
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from '@chakra-ui/react';
 import { useField } from 'formik';
 
+type InputFieldProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  label: string;
+  name: string;
+};
 
-interface InputFieldProps {
+const InputField: React.FC<InputFieldProps> = ({
+  label,
+  size: _,
+  ...props
+}) => {
+  const [field, { error }] = useField(props);
 
-}
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input {...field} {...props} id={field.name} />
+      {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+    </FormControl>
+  );
+};
 
-const InputField: React.FC<InputFieldProps> = ({}) => {
-  const [] = useField();
-  
-  return (<FormControl>
-    <FormLabel htmlFor='username'>Username</FormLabel>
-    <Input
-      value={values.username}
-      onChange={handleChange}
-      id='username'
-      placeholder='username'
-    ></Input>
-    {/* <FormErrorMessage>{form.errors.message}</FormErrorMessage> */}
-  </FormControl>)
-}
-
-export const InputField;
+export default InputField;
