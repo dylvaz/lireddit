144bef6bcc3461658b7674ea9bf461828755f94f
dylvaz <vazquezadylan@gmail.com>
2021-03-24 18:01:30 -0700

    implemneted HOF withUrqlClient in order to make the graphql mutations and queries

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,12 +1,22 @@
+import { withUrqlClient } from 'next-urql';
+
 import NavBar from '../components/NavBar';
+import { createUrqlClient } from '../utils/createUrqlClient';
+import { usePostsQuery } from '../generated/graphql';
 
 const Index = () => {
+  const [{ data }] = usePostsQuery();
   return (
     <>
       <NavBar />
       <div>hello world</div>
+      {!data
+        ? null
+        : data.posts.map((p) => {
+            return <div key={p.id}>{p.title}</div>;
+          })}
     </>
   );
 };
 
-export default Index;
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
