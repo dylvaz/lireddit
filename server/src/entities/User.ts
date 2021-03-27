bcfaf292b40db0ce8432ed22679941eb08e4da58
dylvaz <vazquezadylan@gmail.com>
2021-03-26 20:04:15 -0700

    added an email field to the user entity

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -22,4 +22,8 @@ export class User {
   @Field(() => String)
   @Property({ type: 'date', onUpdate: () => new Date() })
   updatedAt = new Date();
+
+  @Field()
+  @Property({ type: 'text', unique: true })
+  email!: string;
 }
