9e8e90814ff4df7a9356009738160f89a9e1e848
dylvaz <vazquezadylan@gmail.com>
2021-03-26 20:08:44 -0700

    replaced redis with ioredis and added the redis client to the context

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,34 +3,34 @@ import 'reflect-metadata';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
-import redis from 'redis';
+import Redis from 'ioredis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
 import cors from 'cors';
 
 import { PostResolver } from './graphql/resolvers/postResolvers';
-import { _prod_ } from './constants';
+import { COOKIE_NAME, _prod_ } from './constants';
 import mikroOrmConfig from './mikro-orm.config';
 import { UserResolver } from './graphql/resolvers/userResolvers';
 import { MyContext } from './types';
+
 require('dotenv').config({ path: __dirname + '/.env' });
 
 const main = async () => {
   const PORT = process.env.PORT ?? 4000;
-
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
 
   const app = express();
 
   const RedisStore = connectRedis(session);
-  const redisClient = redis.createClient();
+  const redis = new Redis();
   app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
   app.use(
     session({
-      name: 'rqa',
-      store: new RedisStore({ client: redisClient, disableTouch: false }),
+      name: COOKIE_NAME,
+      store: new RedisStore({ client: redis, disableTouch: false }),
       cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
         httpOnly: true,
@@ -48,7 +48,7 @@ const main = async () => {
       resolvers: [PostResolver, UserResolver],
       validate: false,
     }),
-    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res, redis }),
   });
 
   apolloServer.applyMiddleware({ app, cors: false });
