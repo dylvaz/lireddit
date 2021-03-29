ed41f85e3169d5313aeb8d6741a246346b68cadc
dylvaz <vazquezadylan@gmail.com>
2021-03-28 23:22:14 -0700

    created ui and ux for creating a psots - useIsAuth will run to make sure the user is logged in - will make sure that the form input is not empty - gives visual field errors - on submission runs the createPost Mutation

---
+++ client/src/pages/create-post.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Box, Button } from '@chakra-ui/react';
+import { Form, Formik } from 'formik';
+import { useRouter } from 'next/router';
+import { withUrqlClient } from 'next-urql';
+
+import InputField from '../components/InputField';
+import { useCreatePostMutation } from '../generated/graphql';
+import { createUrqlClient } from '../utils/createUrqlClient';
+import Layout from '../components/Layout';
+import { useIsAuth } from '../utils/useIsAuth';
+
+interface CreatePostProps {}
+
+const CreatePost: React.FC<CreatePostProps> = ({}) => {
+  const router = useRouter();
+  useIsAuth();
+  const [, createPost] = useCreatePostMutation();
+  return (
+    <div>
+      <Layout variant='small'>
+        <Formik
+          initialValues={{ title: '', text: '' }}
+          onSubmit={async (values, { setFieldError }) => {
+            if (values.text === '') {
+              setFieldError('text', 'text field cannot be empty');
+            }
+            if (values.title === '') {
+              setFieldError('title', 'title field cannot be empty');
+            } else {
+              const { error } = await createPost({ inputs: values });
+              if (!error) {
+                router.push('/');
+              }
+            }
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <InputField
+                name='title'
+                placeholder='title'
+                label='title'
+                type='text'
+              />
+              <Box mt={4}>
+                <InputField
+                  name='text'
+                  placeholder='text'
+                  label='Text'
+                  type='text'
+                />
+              </Box>
+              <Button
+                mt={4}
+                type='submit'
+                isLoading={isSubmitting}
+                colorScheme='teal'
+              >
+                Create Post
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </Layout>
+    </div>
+  );
+};
+
+export default withUrqlClient(createUrqlClient)(CreatePost);
