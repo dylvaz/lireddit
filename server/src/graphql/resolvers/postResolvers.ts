84e78d86b5259adaa203d059000ed48b6aaf3e06
dylvaz <vazquezadylan@gmail.com>
2021-04-08 18:35:05 -0700

    refactored the text snippet field resolver and posts query
    - textSnippet FieldResolver now only adds ellipis if the post is longer than 50 char
    - added the select sql statment for returning the upvote value for all posts if a user is logged in

--- server/src/graphql/resolvers/postResolvers.ts
+++ server/src/graphql/resolvers/postResolvers.ts
@@ -1,4 +1,3 @@
-import { MyContext } from 'src/types';
 import {
   Arg,
   Ctx,
@@ -15,6 +14,8 @@ import {
 } from 'type-graphql';
 import { getConnection } from 'typeorm';
 
+import { Upvote } from '../../entities/Upvote';
+import { MyContext } from '../../types';
 import { Post } from '../../entities/Post';
 import { isAuth } from '../../middleware/isAuth';
 @ObjectType()
@@ -36,17 +37,28 @@ class PostInput {
 export class PostResolver {
   @FieldResolver(() => String)
   textSnippet(@Root() root: Post) {
-    return root.text.slice(0, 50) + '...';
+    if (root.text.length > 50) {
+      return root.text.slice(0, 50) + ' ...';
+    }
+    return root.text;
   }
+
   @Query(() => PaginatedPosts)
   async posts(
     @Arg('limit', () => Int) limit: number,
-    @Arg('cursor', () => String, { nullable: true }) cursor: string
+    @Arg('cursor', () => String, { nullable: true }) cursor: string,
+    @Ctx() { req }: MyContext
   ): Promise<PaginatedPosts> {
     const realLimit = Math.min(50, limit) + 1;
     const replacements: any[] = [realLimit];
+
+    if (req.session.userId) {
+      replacements.push(req.session.userId);
+    }
+    let cursorIdx = 3;
     if (cursor) {
       replacements.push(new Date(parseInt(cursor)));
+      cursorIdx = replacements.length;
     }
     const posts = await getConnection().query(
       `
@@ -57,10 +69,15 @@ export class PostResolver {
       'email', u.email,
       'createdAt', u."createdAt",
       'updatedAt', u."updatedAt"
-    ) creator
+    ) creator,
+    ${
+      req.session.userId
+        ? '(select value from upvote where "userId" = $2 and "postId" = p.id) "voteStatus"'
+        : 'null as "voteStatus"'
+    }
     from post p
     inner join "user" u on u.id = p."creatorId"
-    ${cursor ? ' where p."createdAt" < $2' : ''}
+    ${cursor ? ` where p."createdAt" < $${cursorIdx}` : ''}
     order by p."createdAt" DESC
     limit $1
     `,
@@ -114,12 +131,67 @@ export class PostResolver {
   }
 
   @Mutation(() => Boolean)
-  async deletePost(@Arg('id', () => Number) id: number): Promise<Boolean> {
+  async deletePost(@Arg('id', () => Int) id: number): Promise<Boolean> {
     try {
       await Post.delete(id);
     } catch (err) {
       console.error(err);
       return false;
+    }
+    return true;
+  }
+
+  @Mutation(() => Boolean)
+  @UseMiddleware(isAuth)
+  async vote(
+    @Arg('postId', () => Int) postId: number,
+    @Arg('value', () => Int) value: number,
+    @Ctx() { req }: MyContext
+  ) {
+    const { userId } = req.session;
+    const isUpvote = value !== -1;
+    const realValue = isUpvote ? 1 : -1;
+    const upvote = await Upvote.findOne({ where: { postId, userId } });
+
+    //the user has voted on the post before and they are changing their value
+    if (upvote && upvote.value !== realValue) {
+      await getConnection().transaction(async (tm) => {
+        await tm.query(
+          `
+          update upvote 
+          set value = $1
+          where "postId" = $2 and "userId" = $3
+          `,
+          [realValue, postId, userId]
+        );
+        await tm.query(
+          `
+          update post 
+          set points = points + $1
+          where id = $2;
+          `,
+          [2 * realValue, postId]
+        );
+      });
+    } else if (!upvote) {
+      //never voted before
+      await getConnection().transaction(async (tm) => {
+        await tm.query(
+          `
+          insert into upvote ("userId", "postId", value)
+          values($1, $2, $3);
+          `,
+          [userId, postId, realValue]
+        );
+        await tm.query(
+          `
+          update post 
+          set points = points + $1
+          where id = $2;
+          `,
+          [realValue, postId]
+        );
+      });
     }
     return true;
   }
