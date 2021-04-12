8a45cac2144bf04cfae14dffe76b5321dd792d30
dylvaz <vazquezadylan@gmail.com>
2021-04-12 04:23:53 -0700

    implemented types for userLoader and upvoteLoader

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { Redis } from 'ioredis';
 
+import { createUserLoader } from './utils/createUserLoader';
+import { createUpvoteLoader } from './utils/createUpvoteLoader';
 declare module 'express-session' {
   export interface SessionData {
     userId: number;
@@ -11,4 +13,6 @@ export type MyContext = {
   req: Request;
   redis: Redis;
   res: Response;
+  userLoader: ReturnType<typeof createUserLoader>;
+  upvoteLoader: ReturnType<typeof createUpvoteLoader>;
 };
